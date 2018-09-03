f995db4df4438fa569109fc9a726e0d55c3fa86d
Marvin Ernst <ernst@cognitio.de>
2018-09-03 14:03:00 +0200

    cms

--- gatsby-config.js
+++ gatsby-config.js
@@ -29,6 +29,20 @@ module.exports = {
           `Lato\:300,400,400i,700` // you can also specify font weights and styles
         ]
       }
-    }
+    },
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        path: `${__dirname}/src/img`,
+        name: 'images',
+      },
+    },
+    {
+      resolve: 'gatsby-plugin-netlify-cms',
+      options: {
+        modulePath: `${__dirname}/src/cms/cms.js`,
+      },
+    },
+    'gatsby-plugin-netlify',
   ],
 }
